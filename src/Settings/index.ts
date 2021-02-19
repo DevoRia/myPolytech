d82b8a73e5f53f5e1f6d36ac21d48073e1d7418f
DevoRia <the.rocky.vadim@gmail.com>
2021-02-19 20:42:08 +0200

    fix logout

--- src/Settings/index.ts
+++ src/Settings/index.ts
@@ -1,5 +1,6 @@
 import AsyncStorage from "@react-native-community/async-storage";
 import * as Notifications from 'expo-notifications';
+import {cancelAllScheduledNotificationsAsync} from "expo-notifications";
 
 export const clearGroupData = async () => {
   await AsyncStorage.removeItem('group')
